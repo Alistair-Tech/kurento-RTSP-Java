ee179e8f13da9d64f138f9607f1f2761a849bc6b
Sahil Siddiq <39989901+valdaarhun@users.noreply.github.com>
2020-06-27 20:42:09 +0530

    Update OneToManyCommApplication.java

--- One-to-ManyComm/src/main/java/com/OnetoManyComm/demo/OneToManyCommApplication.java
+++ One-to-ManyComm/src/main/java/com/OnetoManyComm/demo/OneToManyCommApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OneToManyCommApplication {
-	
+	//The application starts running from here
 	public static void main(String[] args) {
 		SpringApplication.run(OneToManyCommApplication.class, args);
 	}
